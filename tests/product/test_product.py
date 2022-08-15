f5398024752242f34f976f9194cde1dac94f4fef
elton <eltonmorenocl@gmail.com>
2022-08-15 10:51:54 -0300

    feat(req1): implementa funcao test_cria_produto - funcao que verifica se esta correto o preenchidos de todos atributos inicializado no construtor da classe product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = Product(
+        1,
+        'ciencia da computacao',
+        'trybe',
+        '15/08/2022',
+        '15/09/2022',
+        '16',
+        'estudar python'
+    )
+
+    assert mock.id == 1
+    assert mock.nome_da_empresa == 'trybe'
+    assert mock.nome_do_produto == 'ciencia da computacao'
+    assert mock.data_de_fabricacao == '15/08/2022'
+    assert mock.data_de_validade == '15/09/2022'
+    assert mock.numero_de_serie == '16'
+    assert mock.instrucoes_de_armazenamento == 'estudar python'
