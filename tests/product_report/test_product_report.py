d97c9ca9a44232d99d56b6634218edb06ad2506d
elton <eltonmorenocl@gmail.com>
2022-08-16 09:10:29 -0300

    feat(req2): implementa funcao test_relatorio_produto - funcao que verifica se quando instaciar o objeto product o retorno da frase do relatorio esta no formato correto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = Product(
+        2,
+        'fentanyl citrate',
+        'Target Corporation',
+        '2020-12-06',
+        '2023-12-25',
+        'FR29 5951 7573 74OY XKGX 6CSG D20',
+        'instrucao 2'
+    )
+
+    assert mock.__repr__() == (
+            "O produto fentanyl citrate"
+            " fabricado em 2020-12-06"
+            " por Target Corporation com validade"
+            " até 2023-12-25"
+            " precisa ser armazenado instrucao 2."
+        )
