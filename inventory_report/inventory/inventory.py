1780b647c8ce3be33fae7b125a12f943a40e70fe
elton <eltonmorenocl@gmail.com>
2022-08-19 10:52:43 -0300

    feat(req7): implementa classe Inventory para leitura do formato xml e chamar metodo generate para gerar relatorio simples ou completo de acordo com o parametro recebido e cria um metodo estatico import_data para selecionar o formato de acordo com o tipo recebido

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from xml.etree import ElementTree as ET
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -35,8 +36,31 @@ class Inventory:
             return CompleteReport.generate(data)
 
     @staticmethod
+    def read_xml(path, type):
+        data = []
+
+        with open(path, 'r') as file:
+            tree = ET.parse(file)
+            root = list(tree.getroot())
+
+            info_dict = {}
+
+            for i in range(len(root)):
+                for info in root[i]:
+                    info_dict[info.tag] = info.text
+                data.append(info_dict)
+                info_dict = {}
+
+        if type == 'simples':
+            return SimpleReport.generate(data)
+        elif type == 'completo':
+            return CompleteReport.generate(data)
+
+    @staticmethod
     def import_data(path, type):
         if ".csv" in path:
             return Inventory.read_csv(path, type)
         if ".json" in path:
             return Inventory.read_json(path, type)
+        if ".xml" in path:
+            return Inventory.read_xml(path, type)
