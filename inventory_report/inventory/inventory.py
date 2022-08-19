978b4b7290acb0fbf7086d71b1b3cf174e368c76
elton <eltonmorenocl@gmail.com>
2022-08-19 09:40:14 -0300

    feat(req6): implementa classe Inventory para leitura do formato json e chamar metodo generate para gerar relatorio simples ou completo de acordo com o parametro recebido e cria um metodo estatico import_data para selecionar o formato de acordo com o arquivo recebido

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -19,6 +21,22 @@ class Inventory:
             return CompleteReport.generate(data)
 
     @staticmethod
+    def read_json(path, type):
+        data = []
+
+        with open(path, 'r') as file:
+            data_file = json.load(file)
+            for i in data_file:
+                data.append(i)
+
+        if type == 'simples':
+            return SimpleReport.generate(data)
+        elif type == 'completo':
+            return CompleteReport.generate(data)
+
+    @staticmethod
     def import_data(path, type):
         if ".csv" in path:
             return Inventory.read_csv(path, type)
+        if ".json" in path:
+            return Inventory.read_json(path, type)
